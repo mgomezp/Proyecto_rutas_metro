102c502008b8d7e2f2a9716be7844760e4aa07c0
mgomezp <m.gomezp@unimayor.edu.co>
2026-05-31 17:34:07 -0500

    Segundo cambio, se hizo el codigo de las clases pila y tabla hash

--- scr/Estructuras/Pila.java
+++ scr/Estructuras/Pila.java
@@ -1,3 +1,46 @@
-public class Pila {
+package Estructuras;
 
+public class Pila<T> {
+    
+    private ListasEnlazada<T> elementos;
+    
+    public Pila() {
+        this.elementos = new ListasEnlazada<>();
+    }
+    
+    public void apilar(T dato) {
+        elementos.agregar(dato);
+    }
+ 
+    public T desapilar() {
+        if (estaVacia()) {
+            throw new RuntimeException("La pila esta vacia");
+        }
+        return elementos.eliminar(elementos.getTamaño() - 1);
+    }
+    
+    public T cima() {
+        if (estaVacia()) {
+            throw new RuntimeException("La pila esta vacia");
+        }
+        return elementos.obtener(elementos.getTamaño() - 1);
+    }
+    
+    public boolean estaVacia() {
+        return elementos.estaVacia();
+    }
+    
+    public int getTamaño() {
+        return elementos.getTamaño();
+    }
+    
+    public void limpiar() {
+        elementos.limpiar();
+    }
+    
+    @Override
+    public String toString() {
+        return "Pila: " + elementos.toString();
+    }
 }
+
--- scr/Estructuras/TablaHash.java
+++ scr/Estructuras/TablaHash.java
@@ -1,3 +1,120 @@
-public class TablaHash {
+package Estructuras;
 
+public class TablaHash<K, V> {
+    
+    private static final int CAPACIDAD_INICIAL = 16;
+    private static final double FACTOR_CARGA = 0.75;
+    
+    private ListasEnlazada<ParClaveValor<K, V>>[] tabla;
+    private int tamaño;
+    
+    private static class ParClaveValor<K, V> {
+        K clave;
+        V valor;
+        
+        ParClaveValor(K clave, V valor) {
+            this.clave = clave;
+            this.valor = valor;
+        }
+    }
+    
+    public TablaHash() {
+        this.tabla = new ListasEnlazada[CAPACIDAD_INICIAL];
+        this.tamaño = 0;
+    }
+    
+    private int hash(K clave) {
+        return Math.abs(clave.hashCode() % tabla.length);
+    }
+    
+    public void put(K clave, V valor) {
+        int indice = hash(clave);
+        
+        if (tabla[indice] == null) {
+            tabla[indice] = new ListasEnlazada<>();
+        }
+        
+        ListasEnlazada<ParClaveValor<K, V>> lista = tabla[indice];
+        
+        // busca si ya existe la clave para actualizar
+        for (int i = 0; i < lista.getTamaño(); i++) {
+            ParClaveValor<K, V> par = lista.obtener(i);
+            if (par.clave.equals(clave)) {
+                par.valor = valor;
+                return;
+            }
+        }
+        
+        // se inserta un nuevo par
+        lista.agregar(new ParClaveValor<>(clave, valor));
+        tamaño++;
+        
+        if ((double) tamaño / tabla.length > FACTOR_CARGA) {
+            redimensionar();
+        }
+    }
+    
+    private void redimensionar() {
+        ListasEnlazada<ParClaveValor<K, V>>[] tablaVieja = tabla;
+        tabla = new ListasEnlazada[tabla.length * 2];
+        tamaño = 0;
+        
+        for (int i = 0; i < tablaVieja.length; i++) {
+            if (tablaVieja[i] != null) {
+                for (int j = 0; j < tablaVieja[i].getTamaño(); j++) {
+                    ParClaveValor<K, V> par = tablaVieja[i].obtener(j);
+                    put(par.clave, par.valor);
+                }
+            }
+        }
+    }
+    
+    public V get(K clave) {
+        int indice = hash(clave);
+        ListasEnlazada<ParClaveValor<K, V>> lista = tabla[indice];
+        
+        if (lista == null) return null;
+        
+        for (int i = 0; i < lista.getTamaño(); i++) {
+            ParClaveValor<K, V> par = lista.obtener(i);
+            if (par.clave.equals(clave)) {
+                return par.valor;
+            }
+        }
+        return null;
+    }
+    
+    public boolean contieneClave(K clave) {
+        return get(clave) != null;
+    }
+    
+    public boolean eliminar(K clave) {
+        int indice = hash(clave);
+        ListasEnlazada<ParClaveValor<K, V>> lista = tabla[indice];
+        
+        if (lista == null) return false;
+        
+        for (int i = 0; i < lista.getTamaño(); i++) {
+            ParClaveValor<K, V> par = lista.obtener(i);
+            if (par.clave.equals(clave)) {
+                lista.eliminar(i);
+                tamaño--;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public int getTamaño() {
+        return tamaño;
+    }
+    
+    public boolean estaVacia() {
+        return tamaño == 0;
+    }
+    
+    public void limpiar() {
+        tabla = new ListasEnlazada[CAPACIDAD_INICIAL];
+        tamaño = 0;
+    }
 }
