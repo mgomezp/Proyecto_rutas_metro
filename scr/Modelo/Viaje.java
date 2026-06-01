1871c208b9502422d71e44882f866cfebd5341f9
Elkin-jpg <elkin.uni@unimayor.edu.co>
2026-06-01 15:49:25 -0500

    aporte Viaje.java

--- scr/Modelo/Viaje.java
+++ scr/Modelo/Viaje.java
@@ -1,3 +1,59 @@
-public class Viaje {
+package Modelo;
 
+import java.time.LocalDateTime;
+
+public class Viaje {
+    private String codigoViaje;
+    private Estacion estacionOrigen;
+    private Estacion estacionDestino;
+    private LocalDateTime fechaHora;
+    private double costo;
+    private String tipoTarifa;
+
+    public Viaje(String codigoViaje, Estacion estacionOrigen, 
+                Estacion estacionDestino, double costo) {
+        this.codigoViaje = codigoViaje;
+        this.estacionOrigen = estacionOrigen;
+        this.estacionDestino = estacionDestino;
+        this.fechaHora = LocalDateTime.now();
+        this.costo = costo;
+        this.tipoTarifa = "Normal";
+    }
+
+    public String getCodigoViaje() {
+        return codigoViaje;
+    }
+
+    public Estacion getEstacionOrigen() {
+        return estacionOrigen;
+    }
+
+    public Estacion getEstacionDestino() {
+        return estacionDestino;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public String getTipoTarifa() {
+        return tipoTarifa;
+    }
+
+    public void setTipoTarifa(String tipoTarifa) {
+        this.tipoTarifa = tipoTarifa;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Viaje %s: %s -> %s ($%.2f)", 
+                           codigoViaje, 
+                           estacionOrigen.getNombre(),
+                           estacionDestino.getNombre(),
+                           costo);
+    }
 }
